46a30c67051014a1a478acc82bc1a3e1f1962e06
Hamidrezakf <hrk1fard@gmail.com>
2021-01-13 21:06:02 +0330

    create mapDao

--- app/src/main/java/com/hamidreza/maptask/data/local/MapDao.kt
+++ app/src/main/java/com/hamidreza/maptask/data/local/MapDao.kt
@@ -1,7 +1,18 @@
 package com.hamidreza.maptask.data.local
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
 
 @Dao
 interface MapDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertToMap(mapEntity: MapEntity)
+
+    @Query("SELECT * FROM MapEntity")
+    fun getSymbols():LiveData<List<MapEntity>>
+
 }
